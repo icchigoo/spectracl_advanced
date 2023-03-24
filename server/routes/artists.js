4051b493ed12bf3bf3e97baf6d991fcd88f320c3
icchigoo <icchigoo12@gmail.com>
2023-03-25 02:02:27 +0545

    minor changes in dasboard

--- server/routes/artists.js
+++ server/routes/artists.js
@@ -2,34 +2,6 @@ const artist = require("../models/artist");
 
 const router = require("express").Router();
 
-router.get("/getAll", async (req, res) => {
-  const options = {
-    // sort returned documents in ascending order
-    sort: { createdAt: 1 },
-    // Include only the following
-    // projection : {}
-  };
-
-  const cursor = await artist.find(options);
-  if (cursor) {
-    res.status(200).send({ success: true, data: cursor });
-  } else {
-    res.status(200).send({ success: true, msg: "No Data Found" });
-  }
-});
-
-router.get("/getOne/:getOne", async (req, res) => {
-  const filter = { _id: req.params.getOne };
-
-  const cursor = await artist.findOne(filter);
-
-  if (cursor) {
-    res.status(200).send({ success: true, data: cursor });
-  } else {
-    res.status(200).send({ success: true, msg: "No Data Found" });
-  }
-});
-
 router.post("/save", async (req, res) => {
   const newArtist = artist({
     name: req.body.name,
@@ -43,39 +15,22 @@ router.post("/save", async (req, res) => {
   } catch (error) {
     res.status(400).send({ success: false, msg: error });
   }
+;
 });
 
-router.put("/update/:updateId", async (req, res) => {
-  const filter = { _id: req.params.updateId };
+router.get("/getAll", async (req, res) => {
   const options = {
-    upsert: true,
-    new: true,
+    // sort returned documents in ascending order
+    sort: { createdAt: 1 },
+    // Include only the following
+    // projection : {}
   };
-  try {
-    const result = await artist.findOneAndUpdate(
-      filter,
-      {
-        name: req.body.name,
-        imageURL: req.body.imageURL,
-        twitter: req.body.twitter,
-        instagram: req.body.instagram,
-      },
-      options
-    );
-    res.status(200).send({ artist: result });
-  } catch (error) {
-    res.status(400).send({ success: false, msg: error });
-  }
-});
 
-router.delete("/delete/:deleteId", async (req, res) => {
-  const filter = { _id: req.params.deleteId };
-
-  const result = await artist.deleteOne(filter);
-  if (result.deletedCount === 1) {
-    res.status(200).send({ success: true, msg: "Data Deleted" });
+  const cursor = await artist.find(options);
+  if (cursor) {
+    res.status(200).send({ success: true, data: cursor });
   } else {
-    res.status(200).send({ success: false, msg: "Data Not Found" });
+    res.status(200).send({ success: true, msg: "No Data Found" });
   }
 });
 
