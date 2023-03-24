15ebd2c379130a1226106f233e23ec11407d2808
icchigoo <icchigoo12@gmail.com>
2023-03-24 23:25:11 +0545

    alnums,songs and artist data passed to mogodb

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 require("dotenv/config");
-
-
-const cors = require("cors");
 const { default: mongoose } = require("mongoose");
 
 
+const cors = require("cors");
+
+
+
+
+
 app.use(cors({origin : true}));
+app.use(express.json());
 
 
 app.get("/", (req, res) => {
@@ -15,9 +19,23 @@ app.get("/", (req, res) => {
 })
 
 
+
 // user authentication routes
 const userRoute = require("./routes/auth");
 app.use("/api/users/", userRoute);
+
+// Artist route
+const artistsRoute = require("./routes/artists");
+app.use("/api/artists/", artistsRoute);
+
+// Album route
+const albumRoute = require("./routes/albums");
+app.use("/api/albums/", albumRoute);
+
+// Songs route
+const songRoute = require("./routes/songs");
+
+app.use("/api/songs/", songRoute);
 
 
 
