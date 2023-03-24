965fe63359622f87dd34417d0b1038f555c6c6e5
icchigoo <icchigoo12@gmail.com>
2023-03-24 13:22:54 +0545

    moongose connected in backend and token generated in fe

--- server/app.js
+++ server/app.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const app = express();
+require("dotenv/config");
 
 
 const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+
+
+// app.use(cors({origin : true}));
 
 
 app.get("/", (req, res) => {
     return res.json("Hello mugi....")
 })
+
+
+
+mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true });
+mongoose.connection
+  .once("open", () => console.log("Connected"))
+  .on("error", (error) => {
+    console.log(`Error : ${error}`);
+  });
+
+
+
 
 app.listen(4000, () => console.log("listening to port 4000"));
